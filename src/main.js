5031b32e105aeaacd1927161ed1c192ac9d45809
Anton Forzun <antonforzun@gmail.com>
2023-08-11 17:34:08 +0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+// import './style.css'
+import App from './App.vue'
+
+import 'bulma/css/bulma.css'
+import './assets/styles/index.scss'
+
+createApp(App).mount('#app')
---
+++ src/todos.js
@@ -0,0 +1,6 @@
+export default [
+  {id: 1, title: 'HTML + CSS', completed: true},
+  {id: 2, title: 'JS', completed: false},
+  {id: 3, title: 'REACT', completed: false},
+  {id: 4, title: 'VUE', completed: false},
+];
